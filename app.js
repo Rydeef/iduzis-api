6cd310a0381d01d08acad76f6c510a13a2626307
Rydeef <diplyaer03@gmail.com>
2022-07-07 16:44:55 +0300

    changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function startServer() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
-    const PORT = process.env.SERVER_PORT || 3000;
+    const PORT = 3000;
     app.listen(PORT, () =>
       console.log(`App has been started on port ${PORT}`)
     );
